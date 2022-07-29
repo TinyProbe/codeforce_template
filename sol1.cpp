88265bfe1051edca7fa54f39078767fbc5895178
TinyProbe <137adv167@gmail.com>
2022-07-30 07:50:55 +0900

    src file changed something

--- sol1.cpp
+++ sol1.cpp
@@ -1,40 +1,26 @@
 #include <bits/stdc++.h>
 
-#define s_c         static_cast
-#define d_c         dynamic_cast
-#define c_c         const_cast
-#define r_c         reinterpret_cast
-#define m_u         make_unique
-#define m_s         make_shared
-#define m_p         make_pair
 #define mmap        multimap
 #define mset        multiset
 #define u_map       unordered_map
 #define u_set       unordered_set
 #define u_mmap      unordered_multimap
 #define u_mset      unordered_multiset
-#define x           first
-#define y           second
 #define fastio      cin.tie(nullptr)->sync_with_stdio(false)
 #define setpre(d)   cout << fixed << setprecision(d)
-#define pass
 
 using namespace std;
 
-template <class T>          T           rand(const T& = T(1), const T& = T(100));
 template <class T = string> T           scan();
 template <class T = string> vector<T>   scan_vector(const int&);
 template <class T = string> void        print(const T& = T(), const string& = "\n");
 template <class T>          void        print_vector(const vector<T>&, const string& = " ");
+template <class T>          T           _rand(const T& = T(1), const T& = T(100));
 template <class T>          void        _swap(T&, T&);
 template <class T>          T           _gcd(T, T);
 template <class T>          T           _lcm(const T&, const T&);
 template <class T>          T           _ps(const T&);
 
-template <class T> T rand(const T& mn, const T& mx) {
-    static mt19937 gnt((random_device())());
-    return (uniform_int_distribution<T>(mn, mx))(gnt);
-}
 template <class T> T scan() {
     static T tmp;
     cin >> tmp;
@@ -51,6 +37,10 @@ template <class T> void print(const T& obj, const string& end) {
 template <class T> void print_vector(const vector<T>& v, const string& end) {
     for (int i = 0; i < v.size(); ++i) { print(v[i], end); }
     if (end != "\n") { print(); }
+}
+template <class T> T _rand(const T& mn, const T& mx) {
+    static mt19937 gnt((random_device())());
+    return (uniform_int_distribution<T>(mn, mx))(gnt);
 }
 template <class T> void _swap(T& a, T& b) {
     if (&a == &b) { return; }
